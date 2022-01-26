ee9a72816b5d7a093b54cb882a87aeec864ce580
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2022-01-26 22:04:22 +0530

    changes done to run workflow
    Signed-off-by: DISHEN MAKWANA <dishenmakwana.dm@gmail.com>

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders Table  - First Row Task', () => {
-    render(<App />);
-    const linkElement = screen.getByText('hello');
-    expect(linkElement).toBeInTheDocument();
-});
